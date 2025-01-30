9f694b77580895bdf3486529d07d52de8c577d5f
Hassan Arif <hassanarif36368@gmail.com>
2025-01-30 21:33:21 +0500

    First counter project in React

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+let [count,setcount]=useState(0);
+// let count = 15;
+let addvalue = () => {
+  count=count+1;
+  setcount(count);
+  
+}
+let remvevalue = () => {
+if(count>0){
+  count=count-1;
+  setcount(count);
+
+}
+}
+  return (
+    <>
+  <h1>Chai Our React</h1>
+  <h2>Counter: {count}</h2>
+  <button onClick={addvalue}>Add Value</button><br/>
+  <button onClick={remvevalue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
